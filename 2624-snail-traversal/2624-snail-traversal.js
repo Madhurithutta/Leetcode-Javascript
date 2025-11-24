823724f4c57533f351aa7a3d2f80344c0480870e
Madhuri siva durga Tutta <98735325+Madhurithutta@users.noreply.github.com>
2025-11-24 18:55:46 +0530

    Time: 148 ms (94.41%), Space: 69.8 MB (35.25%) - LeetHub

---
+++ 2624-snail-traversal/2624-snail-traversal.js
@@ -0,0 +1,20 @@
+Array.prototype.snail = function(rowsCount, colsCount) {
+    if (this.length !== rowsCount * colsCount) return [];
+
+    const result = Array.from({ length: rowsCount }, () => Array(colsCount).fill(0));
+    let index = 0;
+
+    for (let col = 0; col < colsCount; col++) {
+        if (col % 2 === 0) {
+            for (let row = 0; row < rowsCount; row++) {
+                result[row][col] = this[index++];
+            }
+        } else {
+            for (let row = rowsCount - 1; row >= 0; row--) {
+                result[row][col] = this[index++];
+            }
+        }
+    }
+    return result;
+};
+
