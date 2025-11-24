9c59d4395fd68d286559c3ed92054f17d8ccd3de
Madhuri siva durga Tutta <98735325+Madhurithutta@users.noreply.github.com>
2025-11-24 19:14:58 +0530

    Time: 58 ms (25.75%), Space: 54.3 MB (72.89%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,10 @@
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+        return new Promise((resolve, reject) => {
+            const timer = setTimeout(() => reject("Time Limit Exceeded"), t);
+            fn(...args)
+                .then(res => { clearTimeout(timer); resolve(res); })
+                .catch(err => { clearTimeout(timer); reject(err); });
+        });
+    }
+};
